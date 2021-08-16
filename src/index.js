6a652be484318029c29eb6740fe4232cd92f2fd9
PaulB1985 <paulpriest1985@gmail.com>
2021-08-16 21:46:21 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const stringed = String(n)
+    const reversed = stringed.split('').reverse().join('').split('-').join('')
+    return +reversed
 }
